ca194579f5eff1cdcb8ed77bdea1a1cff46d036e
leop4-cod <leo0411201@gmail.com>
2026-07-12 19:20:46 -0500

    Register Notification in admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Notification
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_teacher', 'is_student', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_teacher', 'is_student', 'is_staff')
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'recipient', 'is_read', 'created_at')
+    search_fields = ('title', 'message')
+    list_filter = ('is_read', 'created_at')
+    readonly_fields = ('created_at',)
